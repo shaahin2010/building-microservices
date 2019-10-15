2a7b90e87e30508a2eeaf43320993c76aa0098a7
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-10-16 01:04:16 +0330

    config server completed

--- building-config-server/src/main/java/com/gmail/shaahin2010/configserver/BuildingConfigServerApplication.java
+++ building-config-server/src/main/java/com/gmail/shaahin2010/configserver/BuildingConfigServerApplication.java
@@ -2,7 +2,10 @@ package com.gmail.shaahin2010.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
 public class BuildingConfigServerApplication {
 
