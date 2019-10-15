48b87b4a01e402009dc40b548b6d026c0fefbb28
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-10-15 23:58:46 +0330

    add config server

---
+++ building-config-server/src/main/java/com/gmail/shaahin2010/configserver/BuildingConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingConfigServerApplication.class, args);
+	}
+
+}
---
+++ building-config-server/src/test/java/com/gmail/shaahin2010/configserver/BuildingConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.shaahin2010.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildingConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
