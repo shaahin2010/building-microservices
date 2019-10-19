c2be1bee8534894814d87068adbe9b2e321f30f6
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-10-19 22:32:25 +0330

    add docker compose file and discovery server

---
+++ building-discovery-server/src/main/java/com/gmail/shaahin2010/buildingdiscoveryserver/BuildingDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.buildingdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ building-discovery-server/src/test/java/com/gmail/shaahin2010/buildingdiscoveryserver/BuildingDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.buildingdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
