2f678c7471d9a6381cdca9f56eb7483644b57fd3
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-12-20 11:47:09 +0330

    add info micro service

---
+++ building-info/src/main/java/com/gmail/shaahin2010/building/infoservice/buildinginfo/BuildingInfoApplication.java
@@ -0,0 +1,17 @@
+package com.gmail.shaahin2010.building.infoservice.buildinginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableJpaRepositories
+public class BuildingInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingInfoApplication.class, args);
+	}
+
+}
---
+++ building-info/src/test/java/com/gmail/shaahin2010/building/infoservice/buildinginfo/BuildingInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.shaahin2010.building.infoservice.buildinginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
